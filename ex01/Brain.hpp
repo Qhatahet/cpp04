043cba93939fa394d38f00a92a20fd5b27be9517
qais <qais>
2025-10-04 10:23:53 +0300

    ex01

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -3,22 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qhatahet <qhatahet@student.42.fr>          +#+  +:+       +#+        */
+/*   By: qais <qais@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 19:26:07 by qhatahet          #+#    #+#             */
-/*   Updated: 2025/10/03 19:36:28 by qhatahet         ###   ########.fr       */
+/*   Updated: 2025/10/03 23:00:53 by qais             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 # define BRAIN_HPP
 
+#include <iostream>
+
 class Brain
 {
 	private:
-		
+		std::string	ideas[100];
 	public:
-		
+		Brain();
+		Brain(const Brain &obj);
+		Brain&	operator=(const Brain &obj);
+		~Brain();
 };
 
 #endif
